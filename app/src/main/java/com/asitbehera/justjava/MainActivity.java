9b8cdaed5f358693a0142599dd94211baad6ffdd
Crazzi_Boii <cool.aryo@gmail.com>
2018-03-22 23:25:17 +0530

    Order & reset button working

--- app/src/main/java/com/asitbehera/justjava/MainActivity.java
+++ app/src/main/java/com/asitbehera/justjava/MainActivity.java
@@ -1,13 +1,49 @@
 package com.asitbehera.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
-
+    int quantity = 5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        quantity = 5;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+    public void resetAll(View view) {
+        quantity = 0;
+        display(quantity);
+        displayPrice(quantity * 5);
+    }
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance(new Locale("en", "in")).format(number));
     }
 }
